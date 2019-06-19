56de2582f24a0c7930ccdd4115c402aaa83be856
Ravi <ravighaghada28@gmail.com>
2019-06-19 17:24:18 +0100

    Improved speed. Can now look at how many clicks it takes to reach destination link.

--- Wikicrawler.py
+++ Wikicrawler.py
@@ -7,7 +7,7 @@ from queue import Queue
 class Wikicrawler:
 
     def __init__(self, start, end):
-        self.visted = {}
+        self.visited = {}
         self.tovisit = Queue()
 
         self.STARTURL = start
@@ -19,51 +19,60 @@ class Wikicrawler:
     def crawlURL(self):
 
         currURL = self.tovisit.get()
-        self.visted[currURL] = " "
 
         connection = urllib.request.urlopen(self.BASEURL.format(currURL))
         dom = lxml.html.fromstring(connection.read())
 
         for link in dom.xpath('//a/@href'):  # select the url in href for all a tags(links)
-            if self.isAcceptableFormat(link):
-                if not self.hasBeenEncountered(link):
+            if self.is_acceptable_format(link):
+                if not self.has_been_encountered(link):
                     print("\n" + link)
+
+                    self.visited[link] = currURL
+
                     if link == self.ENDURL:
                         print("\nEND URL Reached!")
-                        return True
+                        return self.count_retrace(link)
                     self.tovisit.put(link)
-        return False
+        return 0
+
 
     def main(self):
         while not self.tovisit.empty():
             self.crawlURL()
 
-    def isAcceptableFormat(self, link):
+    def is_acceptable_format(self, link):
         link = str(link)
+
+        badwords = ["/Wikipedia:", "/Category:", "/Special:", "/Talk:", "/File:",
+                    "/Template", "/Template_talk:", "/Help:", "/Portal:", "/Book:"]
 
         if not link.startswith("/wiki/"):
             return False
-        if "/Wikipedia:" in link:
-            return False
-        if "/Category:" in link:
-            return False
-        if "/Special:" in link:
-            return False
-        if "/Talk:" in link:
-            return False
+
+        for i in badwords:
+            if i in link:
+                return False
 
         return True
 
-    def hasBeenEncountered(self, link):
+    def has_been_encountered(self, link):
         link = str(link)
-        if link in self.visted:
-            return True
-        if link in self.tovisit.queue:
+        if link in self.visited:
             return True
         return False
 
+    def count_retrace(self, link):
+        if link == self.STARTURL:
+            return 0
+        else:
+            return 1 + self.count_retrace(self.visited[link])
 
 
-a = Wikicrawler("/wiki/Tom_Scott_(entertainer)", "/wiki/London")
-while not a.crawlURL():
-    pass
+
+a = Wikicrawler("/wiki/Planet", "/wiki/Divinity")
+while True:
+    result = a.crawlURL()
+    if result !=0:
+        print(result)
+        break
